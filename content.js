98bf7b3d3af3ca71c5b5c2adaa8a18f001e2d844
deltamaze <wcpooley@gmail.com>
2018-02-22 22:16:19 -0600

    playing with dom elements

---
+++ content.js
@@ -0,0 +1,3 @@
+/* global document */
+const body = document.getElementsByTagName('body');
+//body[0].style.visibility = 'hidden';
--- popup.js
+++ popup.js
@@ -4,7 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const toggleSwitch = document.getElementById('toggleSwitch');
   toggleSwitch.addEventListener('click', () => {
     chrome.tabs.getSelected(null, (tab) => {
-      // const d = document;
+      const d = document;
+      // just update ext settings for on/off status
+      // if tab is in target list, mouse is not on screen/window is not active, then black out
+      console.log(d);
+      console.log(tab);
 
       // var f = d.createElement('form');
       // f.action = 'http://gtmetrix.com/analyze.html?bm';
