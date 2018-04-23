9f2fd8a4fa4b18b2c410ab73051b9d512686c2f3
deltamaze <wcpooley@gmail.com>
2018-04-23 07:36:33 -0500

    setting up settings syncing

--- options.js
+++ options.js
@@ -1,4 +1,30 @@
 // use storage
+let config;
+chrome.storage.sync.get(['targetType', 'siteWhiteList', 'siteBlackList'], function (items) {
+  console.log(items);
+  config = items;
+  if (config === undefined) {
+    console.log("setting up new items object");
+    config =
+      {
+        targetType: ["WhiteList", "BlackList"],
+        siteWhiteList: ["google.com", "test.com"],
+        siteBlackList: ["google.com", "test.com"]
+      }
+    chrome.storage.sync.set(config, function () {
+      console.log('Settings saved');
+    });
+  
+  }
+  console.log(config);
+});
+
+
+
+
+
+
+//save button clicked, sync storage
 // Save it using the Chrome extension storage API.
 // chrome.storage.sync.set({'foo': 'hello', 'bar': 'hi'}, function() {
 //     console.log('Settings saved');
